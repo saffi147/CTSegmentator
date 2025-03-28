9e0bed67a5135ce3e9c2218e01dc28ee5a9dc517
Saffi <saffi.hunt@oncoresmedical.com>
2025-03-28 16:27:11 +0800

    updated repository link

--- ctsegmentator/download_weights.py
+++ ctsegmentator/download_weights.py
@@ -108,7 +108,7 @@ def download_fold_weights_via_api(output_dir, fold_numbers=[0, 1, 2, 3, 4], clea
     output_dir = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
     token = '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'
-    github_repo = "UWA-Medical-Physics-Research-Group/CTSegmentator"
+    github_repo = "saffi147/CTSegmentator"
     current_version = __version__
 
     headers = {
